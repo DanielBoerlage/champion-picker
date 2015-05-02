f0fe44c53a9af41dcfa7e19e6ce930895491000c
tomlevit <tomtomlevit@gmail.com>
2015-05-01 21:00:41 -0700

    lol Daniel's dumb

--- src/championpicker/Main.java
+++ src/championpicker/Main.java
@@ -1,7 +1,22 @@
-package championpicker;
+import java.io.IOException;
+import jcurses.widgets.*;
+import jcurses.system.Toolkit;
+import jcurses.system.CharColor;
 
-class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
+
+class InputTest{
+
+	public static void main(String[] args){
+		Window w = new Window(40, 20, true, "Numbers");
+            DefaultLayoutManager mgr = new DefaultLayoutManager();
+            mgr.bindToContainer(w.getRootPanel());
+
+            CharColor color = new CharColor(CharColor.WHITE, CharColor.GREEN);
+
+            w.show();
+
+            while ( ! w.isClosed() ) {
+                Toolkit.printString( "Hello World!", 45, 17, color);
+            }
 	}
 }
