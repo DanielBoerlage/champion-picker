822c0a81342ada205074f0534444ad2538de2434
DanielBoerlage <daniel.brlg@gmail.com>
2015-05-02 07:41:14 +0100

    please kill me

--- src/championpicker/Main.java
+++ src/championpicker/Main.java
@@ -1,22 +1,24 @@
+package championpicker;
+
 import java.io.IOException;
 import jcurses.widgets.*;
 import jcurses.system.Toolkit;
 import jcurses.system.CharColor;
 
+class Main{
 
-class InputTest{
-
-	public static void main(String[] args){
-		Window w = new Window(40, 20, true, "Numbers");
-            DefaultLayoutManager mgr = new DefaultLayoutManager();
-            mgr.bindToContainer(w.getRootPanel());
-
-            CharColor color = new CharColor(CharColor.WHITE, CharColor.GREEN);
-
-            w.show();
-
-            while ( ! w.isClosed() ) {
-                Toolkit.printString( "Hello World!", 45, 17, color);
-            }
+	public static void main(String[] args) throws Exception {
+		Window w = new Window(40, 20, true, "Hello World Window");
+    DefaultLayoutManager mgr = new DefaultLayoutManager();
+    mgr.bindToContainer(w.getRootPanel());
+    mgr.addWidget(
+		  new Label("Hello World!",
+			    new CharColor(CharColor.WHITE, CharColor.GREEN)),
+		  0, 0, 40, 20,
+		  WidgetsConstants.ALIGNMENT_CENTER,
+		  WidgetsConstants.ALIGNMENT_CENTER);
+    w.show();
+    Thread.currentThread().sleep(5000);	// sleep for 5 seconds, then exit
+    w.close(); // reset the native console
 	}
 }
