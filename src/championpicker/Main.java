22bec8a5d5b40f5246f35429c29d38933bca7885
DanielBoerlage <daniel.brlg@gmail.com>
2015-05-19 13:06:48 -0700

    removed jcurses

--- src/championpicker/Main.java
+++ src/championpicker/Main.java
@@ -1,24 +1,8 @@
 package championpicker;
 
-import java.io.IOException;
-import jcurses.widgets.*;
-import jcurses.system.Toolkit;
-import jcurses.system.CharColor;
-
 class Main{
 
-	public static void main(String[] args) throws Exception {
-		Window w = new Window(40, 20, true, "Hello World Window");
-    DefaultLayoutManager mgr = new DefaultLayoutManager();
-    mgr.bindToContainer(w.getRootPanel());
-    mgr.addWidget(
-		  new Label("Hello World!",
-			    new CharColor(CharColor.WHITE, CharColor.GREEN)),
-		  0, 0, 40, 20,
-		  WidgetsConstants.ALIGNMENT_CENTER,
-		  WidgetsConstants.ALIGNMENT_CENTER);
-    w.show();
-    Thread.currentThread().sleep(5000);	// sleep for 5 seconds, then exit
-    w.close(); // reset the native console
+	public static void main(String[] args) {
+		System.out.println("Hello World");
 	}
 }
