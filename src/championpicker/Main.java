5483cb0247d909d19201d763f5f865ee82d281c1
DanielBoerlage <daniel.brlg@gmail.com>
2015-05-19 13:20:38 -0700

    added lanterna library in build file

--- src/championpicker/Main.java
+++ src/championpicker/Main.java
@@ -1,8 +1,24 @@
 package championpicker;
 
+import com.googlecode.lanterna.gui.GUIScreen;
+import com.googlecode.lanterna.TerminalFacade;
+
+
 class Main{
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+	public static void main(String[] args) throws Exception {
+		GUIScreen textGUI = TerminalFacade.createGUIScreen();
+	    if(textGUI == null) {
+	        System.err.println("Couldn't allocate a terminal!");
+	        return;
+	    }
+	    textGUI.getScreen().startScreen();
+	    textGUI.setTitle("GUI Test");
+
+	    //Do GUI logic here
+
+	    Thread.sleep(3000);
+
+	    textGUI.getScreen().stopScreen();
 	}
 }
